d1e2d1514f61faa698e74d3238ac8fd77179f45b
ddy <1411633401@qq.com>
2025-03-12 13:21:45 +0800

    two pipes

--- Linux/lesson35/2_blockqueue/MainCp.cc
+++ Linux/lesson35/2_blockqueue/MainCp.cc
@@ -4,50 +4,70 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int Add(int x, int y)
+template<class C, class S>
+class BlockQueues
 {
-    return x + y;
-}
+public:
+    BlockQueue<C>* _c_bq;
+    BlockQueue<S>* _s_bq;
+};
 
 void* consumer(void* args)
 {
-    BlockQueue<Task>* bq = static_cast<BlockQueue<Task>*>(args);
+    BlockQueue<CalTask>* c_bq = (static_cast<BlockQueues<CalTask, SaveTask>*>(args))->_c_bq;
+    BlockQueue<SaveTask>* s_bq = (static_cast<BlockQueues<CalTask, SaveTask>*>(args))->_s_bq;
     while (true)
     {
-        Task t;
-        bq->pop(&t);
-        std::cout << t() << std::endl;
-        // int data;
-        // bq->pop(&data);
-        // std::cout << "消费数据: " << data << std::endl;
-        // sleep(1);
+        CalTask t;
+        c_bq->pop(&t);
+        std::string result = std::to_string(t());
+        std::cout << "推送数据 " << result << std::endl;
+        s_bq->push(SaveTask(result, Save));
     }
 }
 
 void* producer(void* args)
 {
-    BlockQueue<Task>* bq = static_cast<BlockQueue<Task>*>(args);
+    BlockQueue<CalTask>* c_bq = (static_cast<BlockQueues<CalTask, SaveTask>*>(args))->_c_bq;
     while (true)
     {
         int x = rand() % 10 + 1;
         int y = rand() % 5 + 1;
-        bq->push(Task(x,y,Add));
-        // int data = rand() % 10 + 1;
-        // bq->push(data);
-        // std::cout << "生产数据: " << data << std::endl;
-        // sleep(1);
+        c_bq->push(CalTask(x,y,Add));
+        sleep(1);
+    }
+}
+
+void* saver(void* args)
+{
+    BlockQueue<SaveTask>* s_bq = (static_cast<BlockQueues<CalTask, SaveTask>*>(args))->_s_bq;
+    while (true)
+    {
+        SaveTask t;
+        s_bq->pop(&t);
+        t();
+        std::cout << "完成保存" << std::endl;
     }
 }
 
 int main()
 {
     srand((unsigned long)time(0) ^ getpid());
-    BlockQueue<Task>* bq = new BlockQueue<Task>();
-    pthread_t c, p;
-    pthread_create(&c, nullptr, consumer, bq);
-    pthread_create(&p, nullptr, producer, bq);
+
+    BlockQueues<CalTask, SaveTask> bqs;
+    bqs._c_bq = new BlockQueue<CalTask>();
+    bqs._s_bq = new BlockQueue<SaveTask>();
+
+    pthread_t c, p, s;
+    pthread_create(&p, nullptr, producer, &bqs);
+    pthread_create(&c, nullptr, consumer, &bqs);
+    pthread_create(&s, nullptr, saver, &bqs);
 
     pthread_join(c, nullptr);
     pthread_join(p, nullptr);
+    pthread_join(s, nullptr);
+
+    delete bqs._c_bq;
+    delete bqs._s_bq;
     return 0;
 }
--- Linux/lesson35/2_blockqueue/Task.hpp
+++ Linux/lesson35/2_blockqueue/Task.hpp
@@ -3,14 +3,14 @@
 #include <iostream>
 #include <functional>
 
-class Task
+class CalTask
 {
 public:
     using func_t = std::function<int(int, int)>;
 public:
-    Task()
+    CalTask()
     {}
-    Task(int x, int y, func_t func) :_x(x), _y(y), _callback(func)
+    CalTask(int x, int y, func_t func) :_x(x), _y(y), _callback(func)
     {}
     int operator()()
     {
@@ -21,3 +21,37 @@ private:
     int _y;
     func_t _callback;
 };
+
+int Add(int x, int y)
+{
+    return x + y;
+}
+
+class SaveTask
+{
+public:
+    using func_t = std::function<void(const std::string&)>;
+public:
+    SaveTask()
+    {
+    }
+    SaveTask(const std::string& message, func_t func) :_message(message), _func(func)
+    {
+    }
+    void operator()()
+    {
+        _func(_message);
+    }
+private:
+    std::string _message;
+    func_t _func;
+};
+
+void Save(const std::string& message)
+{
+    std::string target = "./log.txt";
+    FILE* fp = fopen(target.c_str(), "a+");
+    fputs(message.c_str(), fp);
+    fputs("\n", fp);
+    fclose(fp);
+}
